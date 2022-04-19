64eba585f99e914d847c986add29d3cda84a2222
sehoon27@naver.com <sehoon27@naver.com>
2022-04-19 20:04:01 +0900

    백준 7568번 복습완료

---
+++ Algorithm_boj/7568.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	vector<pair<int, int>>man;
+
+	for (int i = 0; i < n; i++)
+	{
+		int weight, high;
+		cin >> weight >> high;
+		man.push_back(make_pair(weight, high));
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		int rank = 1;
+		for (int j = 0; j < n; j++)
+			if (man[i].first < man[j].first && man[i].second < man[j].second)
+				rank++;
+		cout << rank << ' ';
+	}
+	return 0;
+}

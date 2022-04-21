df4f19a51d29d7b0e6de87b227c8096249889a72
sehoon27@naver.com <sehoon27@naver.com>
2022-04-21 15:00:35 +0900

    백준 10989번

---
+++ Algorithm_boj/10989.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int arr[10001] = { 0, };
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int input;
+		cin >> input;
+		arr[input]++;
+	}
+
+	for (int i = 0; i < 10001; i++)
+		for (int j = 0; j < arr[i]; j++)
+			cout << i << '\n';
+
+	return 0;
+}

650e746e373ab4d0dc9fa57b838b7e60f39ff454
sehoon27@naver.com <sehoon27@naver.com>
2022-05-02 19:16:51 +0900

    백준 11650번

--- Algorithm_boj/11650.cpp
+++ Algorithm_boj/11650.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 int main(void)
 {
-    vector <pair<int, int>>v;
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        int x, y;
-        cin >> x >> y;
-        v.push_back(make_pair(x, y));
-    }
+	vector<pair<int, int>>v;
+	int n;
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+		v.push_back(make_pair(x, y));
+	}
+	sort(v.begin(), v.end());
 
-    sort(v.begin(), v.end());
+	for (int i = 0; i < n; i++)
+		cout << v[i].first << ' ' << v[i].second << '\n';
 
-    for (int i = 0; i < n; i++)
-        cout << v[i].first << ' ' << v[i].second << '\n';
-
-    return 0;
+	return 0;
 }

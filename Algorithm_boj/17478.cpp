c6a3941a395085b8dad624e5eacaa46d71fbb8b3
sehoon27@naver.com <sehoon27@naver.com>
2022-05-03 19:41:38 +0900

    백준 17478번

--- Algorithm_boj/17478.cpp
+++ Algorithm_boj/17478.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void recursion(int n, int cnt);
+void recursion(int n,int cnt);
 
 int main(void)
 {
-	int n, cnt = 0;
+	int n;
 	cin >> n;
-	
+
 	cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다." << '\n';
-	recursion(n, cnt);
+	recursion(n, 0);
+
 
 	return 0;
 }
 
-void recursion(int n, int cnt)
+void recursion(int n,int cnt)
 {
-	int temp = cnt;
 	for (int i = cnt; i > 0; i--)
 		cout << "____";
 	cout << "\"재귀함수가 뭔가요?\"" << '\n';
@@ -24,7 +24,7 @@ void recursion(int n, int cnt)
 	{
 		for (int i = cnt; i > 0; i--)
 			cout << "____";
-		cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\""<<'\n';
+		cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"" << '\n';
 		for (int i = cnt; i > 0; i--)
 			cout << "____";
 		cout << "라고 답변하였지." << '\n';
@@ -39,8 +39,9 @@ void recursion(int n, int cnt)
 	for (int i = cnt; i > 0; i--)
 		cout << "____";
 	cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"" << '\n';
-	recursion(n - 1, ++cnt);
-	for (int i = cnt-1; i > 0; i--)
+	recursion(n - 1,++cnt);
+	cnt--;
+	for (int i = cnt; i > 0; i--)
 		cout << "____";
 	cout << "라고 답변하였지." << '\n';
 }

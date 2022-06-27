7ecf409aa61dc825b84a0557a4c88ee12d8ede43
sehoon27@naver.com <sehoon27@naver.com>
2022-06-27 19:47:08 +0900

    백준 18870번

---
+++ Algorithm_boj/18870.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	vector<int>v, v2;
+	int input, n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> input;
+		v.push_back(input);
+	}
+	v2 = v;
+
+	sort(v.begin(), v.end());
+	unique(v.begin(), v.end());
+
+	for (int i = 0; i < n; i++)
+		cout << lower_bound(v.begin(), v.end(), v2[i]) - v.begin() << ' ';
+
+	return 0;
+}

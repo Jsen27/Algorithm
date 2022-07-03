09fe828790ba880d1058f17e8c932526a3f5e4f9
sehoon27@naver.com <sehoon27@naver.com>
2022-07-03 17:56:22 +0900

    백준 1037번

---
+++ Algorithm_boj/1037.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	vector<int>v;
+	int n, input;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> input;
+		v.push_back(input);
+	}
+	sort(v.begin(), v.end());
+
+	cout << v[0] * v[n - 1];
+
+	return 0;
+}
